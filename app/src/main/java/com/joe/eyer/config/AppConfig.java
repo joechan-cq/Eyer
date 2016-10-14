b90cb91cf9ffa52d0b3bc373f589443e9daa0c75
Joe Chan <1030310877@qq.com>
2016-10-14 15:13:23 +0800

    update

--- app/src/main/java/com/joe/eyer/config/AppConfig.java
+++ app/src/main/java/com/joe/eyer/config/AppConfig.java
@@ -1,11 +1,17 @@
 package com.joe.eyer.config;
 
+import android.os.Environment;
+
+import java.io.File;
+
 /**
  * Description
  * Created by chenqiao on 2016/10/14.
  */
 public class AppConfig {
     private static AppConfig ourInstance;
+    public File dataDirectory;
+    private static final String SUFFIX_DATA = "traineddata";
 
     public static AppConfig getInstance() {
         if (ourInstance == null) {
@@ -19,5 +25,14 @@ public class AppConfig {
     }
 
     private AppConfig() {
+        File root = new File(Environment.getExternalStorageDirectory(), "EyerData");
+        dataDirectory = new File(root, "tessdata");
+        if (!dataDirectory.exists()) {
+            dataDirectory.mkdirs();
+        }
+    }
+
+    public boolean checkDataExist(String lang) {
+        return false;
     }
 }
